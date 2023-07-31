0c024c02e8905fbf23f6da34716e835dd758076f
crissu91 <cristinaf.stefan@gmail.com>
2023-07-31 16:14:52 +0100

    testing new component files are working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang='ts'>
+	import Categories from "../components/Categories.svelte";
+	import Comments from "../components/Comments.svelte";
 	import Main from "../components/Main.svelte";
+	import Users from "../components/Users.svelte";
 </script>
 
 
 <Main/>
+<Comments />
+<Users />
+<Categories />
