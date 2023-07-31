32050dc80024c924f030604edcf067e1e8344a9d
crissu91 <cristinaf.stefan@gmail.com>
2023-07-31 16:14:23 +0100

    add comments component

---
+++ src/components/Comments.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	import { fetchAllComments } from "../../server";
+    import type { Comments } from "./interfaces"
+
+   let comments: Comments[] | [] = [];
+
+   fetchAllComments()
+    .then((data)=>{
+        comments = data!
+    })
+    
+</script>
+
+<ul>
+    {#if comments.length}
+    {#each comments as comments}
+    <li>
+        {comments.body}
+    </li>
+    {/each}
+    {/if}
+</ul>
