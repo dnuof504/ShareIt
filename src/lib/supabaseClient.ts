7ce9791fdcd8453dd473fb4ca224475e25e3f980
Yaroslav Ponomaryov <yaroslavponomaryov@gmail.com>
2023-07-31 16:10:32 +0100

    .env added

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,3 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient('https://jxdsmvzqgqofgdetgfha.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZHNtdnpxZ3FvZmdkZXRnZmhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA3OTMwNTgsImV4cCI6MjAwNjM2OTA1OH0.RBaxbvXgSVp7OCCH4W1_dYmVcAcCjLbmbZJiIz_-G0s')
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
