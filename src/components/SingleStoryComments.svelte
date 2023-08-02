ed5d66c62ca878b7644ef5f0a1d9c3309639ddc3
crissu91 <cristinaf.stefan@gmail.com>
2023-08-02 22:49:53 +0100

    add subscribe functionality to update comments once deleted

--- src/components/SingleStoryComments.svelte
+++ src/components/SingleStoryComments.svelte
@@ -1,32 +1,56 @@
-<script lang='ts'>
-    import { fetchStoryComments, deleteSingleComment } from "../../server";
-    import { getContext } from 'svelte';
-    import type { Comments } from "./interfaces";
-    import { loggedAs } from "../store";
-    import AddComments from "./AddComments.svelte";
-    let user: any = getContext('ourUser')
-    console.log(user)
-    let slug: number = getContext('slug')
-    let comments: Comments [] | [] = [];
-    fetchStoryComments(slug)
-    .then((fetchedComments)=>{
-        comments=fetchedComments!;
-    })
-    function handleClick (comment_id:number){
-        deleteSingleComment(comment_id)
-    }
+<script lang="ts">
+	import { fetchStoryComments, deleteSingleComment } from '../../server';
+	import { getContext, onDestroy, onMount } from 'svelte';
+	import { loggedAs } from '../store';
+	import AddComments from './AddComments.svelte';
+	let storyId: number = getContext('slug');
+	import { comments } from '../store';
+	import { formatDate } from '../utils/utils';
+
+	let renderComments: any = [];
+
+	onMount(() => {
+		fetchStoryComments(storyId).then((responseComments) => {
+			renderComments = responseComments;
+		});
+	});
+
+	const unsubscribe = comments.subscribe(() =>
+		fetchStoryComments(storyId).then((responseComments) => {
+			renderComments = responseComments;
+		})
+	);
+
+	onDestroy(unsubscribe);
+
+	async function handleDelete(comment_id: number) {
+		await deleteSingleComment(comment_id);
+
+		await fetchStoryComments(storyId).then((responseComments) => {
+			renderComments = responseComments;
+		});
+	}
 </script>
+
 <AddComments />
 <ul class="border">
-    {#if comments.length}
-    {#each comments as comments}
-    <li>
-        <b>{comments.username} said:</b> {comments.body} <b>IT HAPPENED</b> {comments.created_at}
-        {#if comments.username === $loggedAs}
-        <button on:click={()=>{handleClick(comments.comment_id)}} type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete Comment</button>
-
-        {/if}
-    </li>
-    {/each}
-    {/if}
+	{#if renderComments.length}
+		{#each renderComments as comment}
+			<li>
+				<b>{comment.username} said:</b>
+				{comment.body} <b>IT HAPPENED</b>
+				{formatDate(comment.created_at)}
+				{#if comment.username === $loggedAs}
+					<button
+						on:click={() => {
+							handleDelete(comment.comment_id);
+						}}
+						type="button"
+						class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>Delete Comment</button
+					>
+				{/if}
+			</li>
+		{/each}
+	{/if}
 </ul>
