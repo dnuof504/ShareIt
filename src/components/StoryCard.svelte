9db21fd42c872edd75753e2fc02f4cf71fbb1b98
Yaroslav Ponomaryov <yaroslavponomaryov@gmail.com>
2023-08-03 09:07:27 +0100

    added link to a single story

--- src/components/StoryCard.svelte
+++ src/components/StoryCard.svelte
@@ -6,7 +6,7 @@
     export let body: string;
 </script>
 
-<Card img={`https://picsum.photos/10${id}`} href="/" horizontal>
+<Card img={`https://picsum.photos/10${id}`} href={`/stories/${id}`} horizontal>
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
       {body.slice(0, 35)}...
