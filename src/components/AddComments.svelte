93001e15d9e7bb235c548ae9d2e2c629197277db
Gary-Huynh <gary.c.huynh@gmail.com>
2023-08-04 13:41:55 +0100

    fixed like button

--- src/components/AddComments.svelte
+++ src/components/AddComments.svelte
@@ -26,7 +26,7 @@
 </script>
 
 <form action="" on:submit|preventDefault={handleSubmit}>
-	<Textarea bind:value={newComment.body} />
+	<Textarea class='w-96' bind:value={newComment.body} />
 	<button
 		type="submit"
 		class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
--- src/components/SingleStory.svelte
+++ src/components/SingleStory.svelte
@@ -8,9 +8,10 @@
 	let newVotes: number;
 
 	import { fetchSingleStory, updateVotes } from '../../server';
+	import { formatDate } from '../utils/utils';
 	fetchSingleStory(slug).then((fetchedStory) => {
 		story = fetchedStory!;
-		newVotes = story[0].votes;
+		newVotes = Number(story[0].votes);
 	});
 
 	async function incrementVotes(votes: number, story_id: number) {
@@ -30,16 +31,16 @@
 		<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl text-center">{story.title}</Heading>
 		<img class="object-cover  rounded-t-lg h-96 md:h-auto md:w-auto md:rounded-none md:rounded-l-lg img md:rounded-r-lg " src={story.img_url} alt="">
 		<P class="mb-3 story-text" color="text-black-500 dark:text-gray-400">{story.body}</P>
-		<P class="mb-3 props-block" color="text-black-500 dark:text-gray-400">
-			<div>Posted at: {story.created_at}</div>
-			<div>Likes: {newVotes}</div>
+		<P class=" flex flow-row props-block story-text" color="text-black-500 dark:text-gray-400">
+			<div>Posted at: {formatDate(story.created_at)}</div>
+			<div>❤ {newVotes}</div>
 			<div>Author: {story.username}</div>
 			<div>Category: {story.category_name}</div>
 		</P>
 		<buttton type="button"
-		class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+		class=" storyButton text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
 		on:click={() => incrementVotes(story.votes, story.story_id)}
-		>Likes 👍
+		>Support ❤
 	</buttton>
     </div>
 
