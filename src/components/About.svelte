02a683836c46c9a4af2f50fc5d0cbd01b3bfc31d
Yaroslav Ponomaryov <yaroslavponomaryov@gmail.com>
2023-08-04 17:09:57 +0100

    Friday final commit

---
+++ src/components/About.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { P } from 'flowbite-svelte'
+</script>
+    <h1>405</h1>
+    <p>Found</p>
+  
+  <P class="mb-3" color="text-black-500 dark:text-grey-400" firstupper
+    >ShareIt is a social media platform developed by the 405 Found team with a unique focus on mental health. The platform provides a safe and supportive space for people to share their thoughts, feelings, and struggles with others, and to connect with those who are going through similar experiences.
+    Users can create anonymous profiles and share their stories, thoughts, and emotions through text, images, and videos. Other users can then respond with comforting messages, support, and advice, creating a community of people who are there for each other in times of need.
+  </P>
+  <P class="mb-3" color="text-black-500 dark:text-grey-400"
+    >ShareIt aims to combat the stigma surrounding mental health by providing a space where people can feel comfortable sharing their thoughts and emotions without fear of judgment. The platform is designed to be easy to use and accessible to anyone who needs it, and the 405 Found team is committed to continuously improving and updating the platform to meet the changing needs of its users.
+  </P>
+  <P class="mb-3" color="text-black-500 dark:text-grey-400"
+  >Overall, ShareIt is a valuable resource for anyone who is struggling with mental health issues or who simply needs a safe and supportive space to share their thoughts and emotions. By providing a platform where people can connect with others who are going through similar experiences, the 405 Found team is helping to create a more compassionate and understanding world.
+</P>
