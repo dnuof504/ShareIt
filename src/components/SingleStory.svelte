07994c21a8325cdeb5ef2dcbcf159c4708d36702
Yaroslav Ponomaryov <yaroslavponomaryov@gmail.com>
2023-08-04 16:55:56 +0100

    general Friday commit

--- src/components/SingleStory.svelte
+++ src/components/SingleStory.svelte
@@ -47,6 +47,14 @@
 			<div>Author: {story.username}</div>
 			<div>Category: {story.category_name}</div>
 		</P>
+		<button on:click={()=>{
+					handleAnalysis(story)
+					}}
+		class=" storyButton text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+		>Use the Power of AI</button>
+		{#if sentimentMessage !== null}
+  		<p>{sentimentMessage}</p>
+			{/if}
 		<buttton type="button"
 		class=" storyButton text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
 		on:click={() => incrementVotes(story.votes, story.story_id)}
