bcf4abdf28ff0f0f47c8f127b9bee288b4275aeb
Gary-Huynh <gary.c.huynh@gmail.com>
2023-08-05 01:48:11 +0100

    Anonymous users can no longer delete stories

--- src/components/StoryCard.svelte
+++ src/components/StoryCard.svelte
@@ -31,7 +31,7 @@
 				class="text-white bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-5"
 				>Read more</Button
 			>
-			{#if $loggedAs === username}
+			{#if $loggedAs === username && username !== "Anonymous"}
 				<Button
 					type="button"
 					class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-5"
